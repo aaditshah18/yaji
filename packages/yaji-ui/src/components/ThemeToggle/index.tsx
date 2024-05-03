aea93f0053a252c53f4fb15a0436fb6cca47e8fe
CK <yao.siq@northeastern.edu>
2024-05-03 17:30:31 -0400

    add: icon props for theme toggle

--- packages/yaji-ui/src/components/ThemeToggle/index.tsx
+++ packages/yaji-ui/src/components/ThemeToggle/index.tsx
@@ -1,8 +1,13 @@
 import useTheme from "../../hooks/useTheme.tsx";
 import { Moon, Sun } from "../Icons";
+import { SVGProps } from "react";
 
 export default function ThemeToggle () {
   const { theme, toggleTheme } = useTheme();
+  const iconProps: SVGProps<SVGSVGElement> = {
+    className: "cursor-pointer hover:opacity-75",
+    onClick: () => toggleTheme(),
+  };
 
-  return theme === "light" ? <Sun onClick={toggleTheme} /> : <Moon onClick={toggleTheme} />;
+  return theme === "light" ? <Sun {...iconProps} /> : <Moon {...iconProps} />;
 }
