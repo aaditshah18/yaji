a4dce6f9001b07aad2d86d42c8759c367aec3b2d
CK <yao.siq@northeastern.edu>
2024-06-29 14:52:34 +0800

    add: add note button

--- packages/yaji-ui/src/App.tsx
+++ packages/yaji-ui/src/App.tsx
@@ -3,8 +3,21 @@ import NoteCard from "./components/Card/Note";
 import GitHubLink from "./components/GitHubLink";
 import ThemeToggle from "./components/ThemeToggle";
 import { DATA_SET } from "./types/dataset.ts";
+import NoteEditDrawer from "./components/Editor/Drawer.tsx";
+import { useState } from "react";
+import { Pen } from "./components/Icons";
 
 function App() {
+  const [isOpenEditDrawer, setIsOpenEditDrawer] = useState(false);
+
+  const toggleEditDrawer = () => {
+    setIsOpenEditDrawer(v => !v);
+  };
+
+  const handleAddClick = () => {
+    setIsOpenEditDrawer(true);
+  };
+
   return (
     <>
       <PageHeader>
@@ -27,6 +40,18 @@ function App() {
           ))}
         </div>
       </div>
+
+      <button
+        className="fixed z-10 bottom-10 lg:bottom-24 right-16 lg:right-96 ms-10 text-sm border-0 rounded-full p-4 shadow-3xl bg-blue-500 dark:bg-blue-400"
+        onClick={handleAddClick}
+      >
+        <Pen className="text-gray-100 dark:text-zinc-200" />
+      </button>
+
+      <NoteEditDrawer
+        isOpen={isOpenEditDrawer}
+        onClose={toggleEditDrawer}
+      />
     </>
   );
 }
